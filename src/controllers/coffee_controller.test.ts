1620df561dce9f8255968c40ada16c0c41cdf9a5
Beanalini <dat826@gmail.com>
2023-11-20 23:20:23 +0000

    feat: add extra testsfor the coffee controller

--- src/controllers/coffee_controller.test.ts
+++ src/controllers/coffee_controller.test.ts
@@ -8,3 +8,23 @@ test("GET /coffee should return correct object", async () => {
     name: "Latte",
   });
 });
+
+test("GET /coffee with no param should return correct object with default coffee name", async () => {
+  const res = await request(app).get("/coffee");
+  expect(res.statusCode).toEqual(200);
+  expect(res.body).toEqual({
+    drinkType: "Coffee",
+    name: "Latte",
+  });
+});
+
+test("GET /coffee with different param returns object with name set to param string", async () => {
+  const res = await request(app)
+    .get("/coffee")
+    .query({ coffeeName: "Espresso" });
+  expect(res.statusCode).toEqual(200);
+  expect(res.body).toEqual({
+    drinkType: "Coffee",
+    name: "Espresso",
+  });
+});
